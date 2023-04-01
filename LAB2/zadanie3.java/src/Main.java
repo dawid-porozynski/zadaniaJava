6c17a06471628cdd0f8ed4c4834ec7c1660983dd
dawid-porozynski <pora13234@gmail.com>
2023-04-01 21:21:42 +0200

    dodano set i get

--- LAB2/zadanie3.java/src/Main.java
+++ LAB2/zadanie3.java/src/Main.java
@@ -1,66 +1,73 @@
-import java.util.Scanner;
+class MyDate {
+    private int day, month, year;
 
-public class Main {
-    static class MyDate{
-        int year=0,month=0,days=0;
+    public MyDate(int day, int month, int year) {
+        validate_Day(day);
+        validate_Month(month);
+        validate_Year(year);
 
-
+        this.day = day;
+        this.month = month;
+        this.year = year;
     }
-//    public static void IsDate(){
-//        MyDate date = new MyDate();
-//
-//        if (date.days>31){
-//            System.out.println("Nie ma takiej daty!");
-//            System.exit(0);
-//        }
-//        if (date.month>12){
-//            System.out.println("Nie ma takiego miesiaca");
-//            System.exit(0);
-//        }
-//        if (date.year<1990 || date.year>2050){
-//            System.out.println("Nie pasuje w krtyeria");
-//            System.exit(0);
-//        }
-//
-//    }
 
-    public static void displayDate(){
-        Scanner scanner = new Scanner(System.in);
-        MyDate date = new MyDate();
-
-        System.out.println("wpisz dzien");
-        date.days = scanner.nextInt();
-
-        System.out.println("wpisz miesiac ");
-        date.month = scanner.nextInt();
-
-        System.out.println("wpisz rok");
-        date.year = scanner.nextInt();
-        if (date.days>31){
-            System.out.println("Nie ma takiej daty!");
-            System.exit(0);
-        }
-        if (date.month>12){
-            System.out.println("Nie ma takiego miesiaca");
-            System.exit(0);
-        }
-        if (date.year<1990 || date.year>2050){
-            System.out.println("Nie pasuje w krtyeria");
-            System.exit(0);
-        }
-        //IsDate();
-        if (date.month<10 && date.days<10){
-            System.out.println("0"+date.days+"/0"+date.month+"/"+date.year);
-            System.exit(0);
-        }if (date.days<10){
-            System.out.println("0"+date.days+"/"+date.month+"/"+date.year);
-            System.exit(0);
-        }if (date.month<10){
-            System.out.println(date.days+"/0"+date.month+"/"+date.year);
-            System.exit(0);
-        }
+    private void validate_Day(int day) {
+        if (day > 31 || day < 1) throw new IllegalArgumentException("Nie ma takiego dnia");
     }
+
+    private void validate_Month(int month) {
+        if (month > 12 || month < 1) throw new IllegalArgumentException("Nie ma takiego miesiaca");
+    }
+
+    private void validate_Year(int year) {
+        if (year > 2050 || year < 1990) throw new IllegalArgumentException("Nie pasuje w kryteria");
+    }
+
+    public int get_Month() {
+        return month;
+    }
+
+    public int get_Day() {
+        return day;
+    }
+
+    public int get_Year() {
+        return year;
+    }
+
+    public void set_Month(int month) {
+        validate_Month(month);
+        this.month = month;
+    }
+
+    public void set_Day(int day) {
+        validate_Day(day);
+        this.day = day;
+    }
+
+    public void set_Year(int year) {
+        validate_Year(year);
+        this.year = year;
+    }
+
+    public String display_Date() {
+        return String.format("%02d/%02d/%d", day, month, year);
+    }
+
+    public String toString() {
+        return this.display_Date();
+    }
+
     public static void main(String[] args) {
-    displayDate();
+        MyDate date = new MyDate(12, 5, 2020);
+        System.out.println(date);
+        System.out.println(date.get_Day());
+        System.out.println(date.get_Year());
+        System.out.println(date.get_Month());
+
+        date.set_Day(11);
+        date.set_Month(1);
+        date.set_Year(2011);
+        System.out.println(date.display_Date());
     }
 }
